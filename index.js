45159d2dd3a343b7a40e3f58881bd2052035c7d3
42zeroo <42zeroo@gmail.com>
2021-04-09 01:57:42 +0200

    Changed download method

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import ytdl from 'ytdl-core'
 import fs from 'fs'
 import https from 'https'
+import {DownloaderHelper} from 'node-downloader-helper'
+import download from 'download'
 
 const app = express();
 const PORT = 5000;
@@ -11,23 +13,50 @@ app.get('/:videoID', async(req, res) => {
 
     let videoInfo = await ytdl.getInfo(videoID).catch(e => console.log(e));
     const downloadLinks = ytdl.filterFormats(videoInfo.formats, 'audioonly')
-        .filter(f => !f.hasVideo && f.hasAudio && f.audioQuality === 'AUDIO_QUALITY_MEDIUM')
+        .filter(f => !f.hasVideo && f.hasAudio && f.audioQuality !== 'AUDIO_QUALITY_LOW')
         .map(f => f = f.url);
 
-    const titleWithoutSpaces = videoInfo.videoDetails.title.toLowerCase().replace(/[^a-z']/g, '_');
-    const audioPath = `/download_cache/${titleWithoutSpaces}.mp3`;
+    const formatedTitle = videoInfo.videoDetails.title.toLowerCase().replace(/[^a-z]/g, '_');
+    const audioPath = `/download_cache/${formatedTitle}.mp3`;
+    console.log(downloadLinks)
+
+    try{
+        const dl = new DownloaderHelper(downloadLinks[0], `./download_cache/`, {
+            fileName: `${formatedTitle}.mp3`,
+        });
+        
+        dl.on('end', () => {
+            console.log('Downloading ' +videoInfo.videoDetails.title +  ' Completed');
+            res.download(`${fs.realpathSync('.')}${audioPath}`,
+             ()=>fs.unlinkSync(`${fs.realpathSync('.')}${audioPath}`))
+        })
+        dl.on('progress', (downloadInfo) => console.log("progres: " +downloadInfo.progress))
+        dl.start();
+    }catch(e){
+        if (e) throw e;
+    }
+
+    // try{
+    //     await download(downloadLinks[0], audioPath)
+    //     .then(()=>{
+    //         res.send("Pobieranie " + videoInfo.videoDetails.title);
+    //        // fs.unlinkSync(`${fs.realpathSync('.')}${audioPath}`);
+    //     }).catch(err => console.log(err))
+    // } catch (e) {
+    //     if(e) throw e
+    // }
     
-    const file = fs.createWriteStream('.'+audioPath);
 
-    await https.get(downloadLinks[0], response => response.pipe(file))
+    // const file = fs.createWriteStream('.'+audioPath);
 
-    await res.download(`${fs.realpathSync('.')}${audioPath}`)
+    // https.get(downloadLinks[0], response => response.pipe(file))
 
-    try {
-        fs.unlinkSync(`${fs.realpathSync('.')}${audioPath}`)
-      } catch(err) {
-        console.error(err)
-      }
+     
+        
+
+
+    
+  
 })
 
 app.listen(PORT, ()=> console.log(`server runnin on port http://localhost:${PORT}`))
