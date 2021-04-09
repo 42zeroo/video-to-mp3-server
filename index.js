48bd52efd8140d87188333496624d0d88144dddd
42zeroo <42zeroo@gmail.com>
2021-04-09 02:00:34 +0200

    Changed download method

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import ytdl from 'ytdl-core'
 import fs from 'fs'
-import https from 'https'
 import {DownloaderHelper} from 'node-downloader-helper'
-import download from 'download'
 
 const app = express();
 const PORT = 5000;
@@ -18,13 +16,11 @@ app.get('/:videoID', async(req, res) => {
 
     const formatedTitle = videoInfo.videoDetails.title.toLowerCase().replace(/[^a-z]/g, '_');
     const audioPath = `/download_cache/${formatedTitle}.mp3`;
-    console.log(downloadLinks)
 
     try{
         const dl = new DownloaderHelper(downloadLinks[0], `./download_cache/`, {
             fileName: `${formatedTitle}.mp3`,
         });
-        
         dl.on('end', () => {
             console.log('Downloading ' +videoInfo.videoDetails.title +  ' Completed');
             res.download(`${fs.realpathSync('.')}${audioPath}`,
@@ -35,28 +31,6 @@ app.get('/:videoID', async(req, res) => {
     }catch(e){
         if (e) throw e;
     }
-
-    // try{
-    //     await download(downloadLinks[0], audioPath)
-    //     .then(()=>{
-    //         res.send("Pobieranie " + videoInfo.videoDetails.title);
-    //        // fs.unlinkSync(`${fs.realpathSync('.')}${audioPath}`);
-    //     }).catch(err => console.log(err))
-    // } catch (e) {
-    //     if(e) throw e
-    // }
-    
-
-    // const file = fs.createWriteStream('.'+audioPath);
-
-    // https.get(downloadLinks[0], response => response.pipe(file))
-
-     
-        
-
-
-    
-  
 })
 
 app.listen(PORT, ()=> console.log(`server runnin on port http://localhost:${PORT}`))
